416e052d8e98066070aad840bd7a74ed1c5a89d3
artur <sro.artur@yandex.ru>
2022-11-28 21:57:18 +0300

    Seminar1HW3Example6

---
+++ Seminar1/HW.Example6/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите целое число");
+int num = Convert.ToInt32(Console.ReadLine());
+if ((num % 2) == 0)
+{
+    Console.WriteLine("Это чётное число");
+}
+else
+{
+    Console.WriteLine("Это нечётное число");
+}
