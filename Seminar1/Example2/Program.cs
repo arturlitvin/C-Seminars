e6a4763c5e43d8c1bb60717f0f19bb4fff9dc0fd
artur <sro.artur@yandex.ru>
2022-11-28 18:53:37 +0300

    Seminar1Example12

--- Seminar1/Example2/Program.cs
+++ Seminar1/Example2/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-
+﻿Console.WriteLine("Введите первое целое число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе целое число, отличное от первого");
+int num2 = Convert.ToInt32(Console.ReadLine());
+if (num1 == num2)
+{
+    Console.WriteLine("Вы ввели одинаковые числа");
+}
+else if
+(num2 == num1 / num2)
+{
+    Console.WriteLine("Первое число является квадратом второго");
+}
+else if (num1 == num2 / num1)
+{
+    Console.WriteLine("Второе число является квадратом первого");
+}
+else
+{
+    Console.WriteLine("Квадраты чисел отсутствуют");
+}
