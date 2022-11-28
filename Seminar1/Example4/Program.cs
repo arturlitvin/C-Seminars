3f86276d67bedc90f06f6152033c728b3d777bb3
artur <sro.artur@yandex.ru>
2022-11-28 19:15:34 +0300

    Seminar1Example4commit1

---
+++ Seminar1/Example4/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine ("Enter a three-digit integer number");
+int number = Convert.ToInt32(Console.ReadLine( ));
+int lastDigit = number % 10;//Оператор остатка % вычисляет остаток от деления левого операнда на правый.
+Console.WriteLine ($"Last digit of you number is {lastDigit}");
