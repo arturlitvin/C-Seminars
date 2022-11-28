a0182c19514f15d86f3ed7d8f7d62518154d934f
artur <sro.artur@yandex.ru>
2022-11-28 23:00:54 +0300

    Seminar1HW3Example8

---
+++ Seminar1/HW.Example8/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Введите целое число");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num == 1)
+{
+    Console.WriteLine("В указанном диапазоне нет четных чисел");
+}
+
+else if (num > 0)
+{
+    int print = 2;
+    while (print <= num) // выводит все числа от введённого до 25
+    {
+        Console.Write($"{print} ");
+        print += 2;
+    }
+}
+else
+{
+    int print = 0;
+    while (print >= num) // выводит все числа от введённого до 25
+    {
+        Console.Write($"{print} ");
+        print = print - 2;
+    }
+}
