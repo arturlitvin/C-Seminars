81b330a8cf11237c76b4b4f45e289e1513fa873b
artur <sro.artur@yandex.ru>
2022-11-28 21:32:12 +0300

    Seminar1HW2Example4.1

---
+++ Seminar1/HW.Example4/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите первое целое число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе целое число");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье целое число");
+int num3 = Convert.ToInt32(Console.ReadLine());
+int max=num1;
+if (max <= num2) max=num2;
+if (max <= num3) max=num3;
+Console.WriteLine($"Максимальое число = {max}");
+
+
