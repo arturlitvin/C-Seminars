86210f2d7099b33af5eddfdf1f68bf2331ff6466
artur <sro.artur@yandex.ru>
2022-11-28 19:02:38 +0300

    Seminar1Example3commit1

---
+++ Seminar1/Example3/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите целое число");
+int num = Convert.ToInt32(Console.ReadLine()); //конвертируем строку в числовое значение, т.к. информация считанная с терминала это всегда строковый тип данных
+int minusnum = -num;
+while (minusnum <= num) // выводит все числа от введённого до 25
+{
+    Console.Write($"{minusnum} ");
+    minusnum++;
+}
+Console.WriteLine();
