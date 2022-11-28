a56cba33a9892c021f10a4a362f39fdaef76c841
artur <sro.artur@yandex.ru>
2022-11-28 20:25:29 +0300

    Seminar1Example5commit1

---
+++ Seminar1/Example5/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Enter an integer number");
+int number = Convert.ToInt32(Console.ReadLine());
+int x = 0;
+if (number > 99)
+{
+    while (number >= 1000)
+    {
+        number /= 10;
+        x = number % 10;
+    }
+    Console.WriteLine($"the third number from the left is {x}");
+}
+else
+{
+    Console.WriteLine($"Нифига, тут всего 2 знака");
+}
+
+// разобраться с задачей и придумать второй вариант решения
