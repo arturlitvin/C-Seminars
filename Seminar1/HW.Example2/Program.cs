5f5b8aef6198681848ea06b4e9ae4761a4b579e2
artur <sro.artur@yandex.ru>
2022-11-28 20:53:43 +0300

    Seminar1HW1Example2

---
+++ Seminar1/HW.Example2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите первое целое число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе целое число, отличное от первого");
+int num2 = Convert.ToInt32(Console.ReadLine());
+if (num1 == num2)
+{
+    Console.WriteLine($"Вы ввели ввели два одинаковых числа, они равны");
+}
+else if (num1>num2)
+{
+    Console.WriteLine($"Максимальное число: {num1}, Минимальное число: {num2}");
+}
+else 
+{
+    Console.WriteLine($"Максимальное число: {num2}, Минимальное число: {num1}");
+}
