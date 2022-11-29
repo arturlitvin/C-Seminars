d9e5a89b64fe1b6a5dda5bd1e9490170fd51f8d2
artur <sro.artur@yandex.ru>
2022-11-29 22:59:57 +0300

    Sem2Example4

---
+++ Seminar2/Example4/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите целое число");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num % (7 * 23) == 0)
+{
+Console.WriteLine("Число одновременно кратно числам 7 и 23");
+}
+else
+{
+    Console.WriteLine("Число не кратно числам 7 и 23 одновременно");
+}
+
+/// Остановился на 2-м семинаре C# 1:20:29 sec
