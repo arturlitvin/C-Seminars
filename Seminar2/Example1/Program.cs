eb82410fd0162eda3e2efcebc40d59825dcd1825
artur <sro.artur@yandex.ru>
2022-11-29 20:01:42 +0300

    Sem2Example1

---
+++ Seminar2/Example1/Program.cs
@@ -0,0 +1,5 @@
+﻿int num = new Random().Next(10, 100);
+Console.WriteLine($"Сгенерировано случайное число: {num}");
+int max = num / 10;
+if (num % 10 > max) max = num % 10;
+Console.WriteLine($"Наибольшая цифра в этом числе это: {max}");
