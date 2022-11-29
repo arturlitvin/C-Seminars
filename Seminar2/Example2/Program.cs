74be32a57850b91511e905482fd425ac36408a83
artur <sro.artur@yandex.ru>
2022-11-29 22:32:14 +0300

    Sem2Example3.1

---
+++ Seminar2/Example2/Program.cs
@@ -0,0 +1,14 @@
+﻿// int num = new Random().Next(100, 1000);
+// Console.WriteLine($"Сгенерировано случайное число: {num}");
+// int first = num / 100;
+// int third = num % 10;
+// int newnum = first * 10 + third;
+// Console.WriteLine(newnum);
+
+// Выше моё решение, ниже с урока. Оба верные.
+
+int num = new Random().Next(100, 1000);
+Console.WriteLine($"Сгенерировано случайное число: {num}");
+int first = num / 100;
+int third = num % 10;
+Console.WriteLine($"{first}{third}");
