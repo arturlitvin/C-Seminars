99c0501d0ecfb1065632289850adb31cc940d1ef
artur <sro.artur@yandex.ru>
2022-11-30 21:50:26 +0300

    HW.Fizz_Bizz1.2

--- Seminar2/HW.Example13/Program.cs
+++ Seminar2/HW.Example13/Program.cs
@@ -1,17 +1,16 @@
-﻿Console.WriteLine("Введите натуральное целое число");
+﻿Console.WriteLine("Введите целое число");
 int number = Convert.ToInt32(Console.ReadLine());
-if (number > 99||number<-99)
+if (number > 99 || number < -99)
 {
-    while (number >= 1000||number<=-1000)
+    while (number >= 1000 || number <= -1000)
     {
         number = number / 10;
     }
     int x = number % 10;
-    if (x<0) x=-x;
-    
-    Console.WriteLine($"Третья цифра слева: {x}");
+    if (x < 0)
+    {
+        x = -x;
+        Console.WriteLine($"Третья цифра слева: {x}");
+    }
 }
-else
-{
-    Console.WriteLine($"Третья цифра слева отсутствует");
-}
+else Console.WriteLine($"Третья цифра слева отсутствует");
