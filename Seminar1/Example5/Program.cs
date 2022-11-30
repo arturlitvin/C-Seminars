569655d0363aba80a54a12dc5b59d5de9cc60a14
artur <sro.artur@yandex.ru>
2022-11-30 19:57:59 +0300

    HW.Example15

--- Seminar1/Example5/Program.cs
+++ Seminar1/Example5/Program.cs
@@ -1,12 +1,30 @@
-﻿Console.WriteLine("Enter an integer number");
+﻿// Console.WriteLine("Enter an integer number");
+// int number = Convert.ToInt32(Console.ReadLine());
+// int x = 0;
+// if (number > 99)
+// {
+//     while (number >= 1000)
+//     {
+//         number /= 10;
+//         x = number % 10;
+//     }
+//     Console.WriteLine($"the third number from the left is {x}");
+// }
+// else
+// {
+//     Console.WriteLine($"Нифига, тут всего 2 знака");
+// }
+
+Console.WriteLine("Enter an integer number");
 int number = Convert.ToInt32(Console.ReadLine());
-int x = 0;
+int x = 0, y = 0;
 if (number > 99)
 {
+    y = number;
     while (number >= 1000)
     {
-        number /= 10;
-        x = number % 10;
+        y /= 10;
+        x = y % 10;
     }
     Console.WriteLine($"the third number from the left is {x}");
 }
@@ -14,5 +32,3 @@ else
 {
     Console.WriteLine($"Нифига, тут всего 2 знака");
 }
-
-// разобраться с задачей и придумать второй вариант решения
---
+++ Seminar2/HW.Example13/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Введите целое число");
+int number = Convert.ToInt32(Console.ReadLine());
+int x = 0;
+
+if (number > 999)
+{
+    while (number >= 1000)
+    {
+        number = number / 10;
+        x = number % 10;
+    }
+    Console.WriteLine($"Третья цифра слева: {x}");
+}
+else if (number>99&&number<=999)
+   {
+     x = number % 10;
+    Console.WriteLine($"Третья цифра слева: {x}");
+   }
+else
+{
+    Console.WriteLine($"Третья цифра слева отсутствует");
+}
