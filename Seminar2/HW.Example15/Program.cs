801a16d9e6a6f95047a704b2cb5d08e3e865714e
artur <sro.artur@yandex.ru>
2022-11-30 19:58:52 +0300

    HW.Example15.q

---
+++ Seminar2/HW.Example15/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Введите день недели цифрой (от 1 до 7):");
+int day = Convert.ToInt32(Console.ReadLine());
+if (day < 1 || day > 7) Console.WriteLine("Введенноё число не соответствует условиям");
+else if (day > 0 && day < 6) Console.WriteLine("Это будний день");
+else Console.WriteLine("Выходной");
---
+++ Seminar2/HW.FIZZ_BIZZ/Program.cs
@@ -0,0 +1,8 @@
+﻿int num=1;
+
+while (num<=100) 
+{ 
+    if (num=num/3) Console.Write ("Fizz");
+    else    Console.Write ($"{num} ");
+num++;
+}
