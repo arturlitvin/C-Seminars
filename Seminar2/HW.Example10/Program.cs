ffa73e10b1cf0416c222a9372eba2c572d46df12
artur <sro.artur@yandex.ru>
2022-11-30 23:19:46 +0300

    HW.Sem2.11

--- Seminar2/HW.Example10/Program.cs
+++ Seminar2/HW.Example10/Program.cs
@@ -1,6 +1,6 @@
 ﻿Console.WriteLine("Введите трёхзначное число");
 int num = Convert.ToInt32(Console.ReadLine());
 int second = num % 100 / 10;
-if ((num < 100 || num > 999) && (num > (-100) || num < (-999))) Console.WriteLine("Введенное число не соответствует условиям");
+if ((num < 100 || num > 999) && (num > -100 || num < -999)) Console.WriteLine("Введенное число не соответствует условиям");
 else if (second < 0) Console.WriteLine($"Вторая цифра введённого числа:{-second}");
 else Console.WriteLine($"Вторая цифра введённого числа:{second}");
