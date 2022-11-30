5f4a0526864a63e3641afefcbc8549961163cdee
artur <sro.artur@yandex.ru>
2022-11-30 23:09:49 +0300

    HW.pro.2

--- Seminar2/HW.FIZZ_BIZZ/Program.cs
+++ Seminar2/HW.FIZZ_BIZZ/Program.cs
@@ -1,8 +1,9 @@
-﻿int num=1;
-while (num<=100) 
-{   if(num%15==0) Console.Write ("FizzBuzz ");
-    else if (num%3==0) Console.Write ("Fizz ");
-    else if(num%5==0) Console.Write ("Buzz ");
-    else    Console.Write ($"{num} ");
-num++;
+﻿int num = 1;
+while (num <= 100)
+{
+    if (num % 15 == 0) Console.Write("FizzBuzz ");
+    else if (num % 3 == 0) Console.Write("Fizz ");
+    else if (num % 5 == 0) Console.Write("Buzz ");
+    else Console.Write($"{num} ");
+    num++;
 }
