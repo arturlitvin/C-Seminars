5ff6c69b03a1a344e18e6be8e235d3562d38cb7e
artur <sro.artur@yandex.ru>
2022-11-30 14:15:05 +0300

    Sem2Example5.1

---
+++ Seminar2/Example5/Program.cs
@@ -0,0 +1,19 @@
+﻿// Console.WriteLine("Введите первое целое число");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе целое число");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// if (num1 /num2 == num2) 
+// Console.WriteLine("Первое число является квадратом второго");
+// else if (num2 / num1 == num1)
+// Console.WriteLine("Второе число является квадратом первого");
+// else Console.WriteLine("Квадраты чисел отсутствуют"); 
+
+//Выше моё решение, рабочее...Не знал про команду(||)-или
+
+Console.WriteLine("Введите первое целое число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе целое число");
+int num2 = Convert.ToInt32(Console.ReadLine());
+if (num1 /num2 == num2 || num2 / num1 == num1) // || - или
+Console.WriteLine("Одно из чисел является квадратом другого");
+else Console.WriteLine("Квадраты чисел отсутствуют");
