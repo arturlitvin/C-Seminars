855650781523fd371fabca3619a427a6a7dc8073
artur <sro.artur@yandex.ru>
2022-12-02 22:00:40 +0300

    Seminar3HW19.2

--- Seminar3/Example4/Program.cs
+++ Seminar3/Example4/Program.cs
@@ -23,7 +23,7 @@ Console.WriteLine("введите целое число");
 int n = Convert.ToInt32(Console.ReadLine());
 for (int i=1; i <= n;i++)
 {
-    Console.Write($"{i*i} ");
+    Console.Write($"{i*i*i} ");
 }
 }
 catch
--- Seminar3/HW.Example19/Program.cs
+++ Seminar3/HW.Example19/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите пятизначное число");
+int n = Convert.ToInt32(Console.ReadLine());
+int n1 = n/10000 % 10;
+int n2 = n/1000%10;
+int n4 = n/10%10;
+int n5 = n%10;
+if( n>99999 || n<10000) Console.WriteLine("Неверное число");
+else if (n1==n5 && n2==n4) Console.WriteLine("Паллиндром");
+else Console.WriteLine("не паллиндром");   
