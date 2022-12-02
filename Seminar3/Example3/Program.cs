8173494e088f5755a2b6ed0a0964fa48a59be8bd
artur <sro.artur@yandex.ru>
2022-12-02 17:13:04 +0300

    Seminar3HW19.1

---
+++ Seminar3/Example3/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+
+// A (3,6); B (2,1) -> 5,09
+// A (7,-5); B (1,-1) -> 7,21
+// d = √ [(x2-x1)2+ (y2-y1)2]
+try
+{
+    Console.WriteLine("Введите координаты четырёх точек");
+    int x1 = Convert.ToInt32(Console.ReadLine());
+    int y1 = Convert.ToInt32(Console.ReadLine());
+    int x2 = Convert.ToInt32(Console.ReadLine());
+    int y2 = Convert.ToInt32(Console.ReadLine());
+    double r = distance(x1, y1, x2, y2);
+    Console.WriteLine($"Расстояние между точками равно {r}");
+}
+catch
+{
+    System.Console.WriteLine("Вы ввели не числовое значение");
+}
+double distance(int x1, int y1, int x2, int y2)
+{
+
+    double d = Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+    return d;
+}
+
---
+++ Seminar3/Example4/Program.cs
@@ -0,0 +1,32 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел
+// от 1 до N.
+// 5 -> 1, 4, 9, 16, 25.
+// 2 -> 1,4
+
+/* Решение в строку(моё)
+
+Console.WriteLine("введите целое число");
+int num = Convert.ToInt32(Console.ReadLine());
+int square = 0;
+int first = 1;
+while (first <= num)
+{
+    square=first*first;
+    Console.Write($"{square} ");
+    first++;
+}
+*/
+// Решение с урока:
+try
+{
+Console.WriteLine("введите целое число");
+int n = Convert.ToInt32(Console.ReadLine());
+for (int i=1; i <= n;i++)
+{
+    Console.Write($"{i*i} ");
+}
+}
+catch
+{
+    Console.WriteLine("вы что-то сделали не так");
+}
