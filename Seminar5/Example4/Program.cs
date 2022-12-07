636265620ed7459b7ae10699d5abef1e2d999259
artur <sro.artur@yandex.ru>
2022-12-07 22:07:12 +0300

    S5E5

---
+++ Seminar5/Example4/Program.cs
@@ -0,0 +1,49 @@
+﻿// Задача 37: Найдите произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент, второй и предпоследний и т.д. Результат запишите в новом массиве.
+// [1 2 3 4 5] -> 5 8 3
+// [6 7 3 6] -> 36 21
+
+int[] CreateArray(int size)  // принимает размер массива и возвращает сгенерированный массив
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+        array[i] = new Random().Next(-9, 10);
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    foreach (int el in array)   //этот цикл только для просмотра и тут нет индексов
+        Console.Write($"{el} ");
+    // for (int i = 0; i < array.Length; i++)
+    //     Console.Write($"{array[i]} ");
+    Console.WriteLine();
+}
+int[] NewArray(int[] array)
+{
+    int size = array.Length / 2;
+
+    if (array.Length % 2 == 1)
+        size = array.Length / 2 + 1;
+        int[] newArray = new int[size];
+
+    for (int i = 0; i < size; i++)
+        newArray[i] = array[i]*array[array.Length - i - 1];
+        if (array.Length%2>0) newArray[size-1]=array[array.Length/2];
+    return newArray;
+}
+
+// void NewArr(int[] newArray)
+// {
+//     foreach (int el in newArray)   //этот цикл только для просмотра и тут нет индексов
+//         Console.Write($"{el} ");
+//     // for (int i = 0; i < array.Length; i++)
+//     //     Console.Write($"{array[i]} ");
+//     Console.WriteLine();
+// }
+
+Console.WriteLine("Введите размерность массива");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] array = CreateArray(size);
+PrintArray(array);
+Console.WriteLine();
+PrintArray (NewArray(array));
---
+++ Seminar5/Example5/Program.cs
@@ -0,0 +1,38 @@
+﻿//Задача 35: Задайте одномерный массив из 15 случайных чисел от -100 до 100. Найдите количество элементов массива, значения которых лежат в отрезке [10,99].
+
+int[] CreateArray(int size)  // принимает размер массива и возвращает сгенерированный массив
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+        array[i] = new Random().Next(-100, 101);
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    foreach (int el in array)   //этот цикл только для просмотра и тут нет индексов
+        Console.Write($"{el} ");
+    // for (int i = 0; i < array.Length; i++)
+    //     Console.Write($"{array[i]} ");
+    Console.WriteLine();
+}
+
+// void PrintCount(int[] array) // void ничего не возвращает!
+// {
+//     int count = 0;
+//     for (int i = 0; i < array.Length; i++)
+//         if (array[i] >= 10 && array[i] <= 99) count ++;
+//     Console.WriteLine($"количество элементов массива, значения которых лежат в отрезке [10,99] {count} ");
+// }
+int PrintCount(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+        if (array[i] >= 10 && array[i] <= 99) count ++;
+    return count;
+}
+
+int size = 15;
+int[] array = CreateArray(size);
+PrintArray(array);
+Console.WriteLine(PrintCount(array));
