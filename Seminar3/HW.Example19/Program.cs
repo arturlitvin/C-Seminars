82e45c6566c779171e1d42ac09d697834178986b
artur <sro.artur@yandex.ru>
2022-12-11 17:03:20 +0300

    S4.HW

--- Seminar3/HW.Example19/Program.cs
+++ Seminar3/HW.Example19/Program.cs
@@ -1,9 +1,18 @@
-﻿Console.WriteLine("Введите пятизначное число");
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом. Через строку решать нельзя.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+void CheckPal(int n)
+{
+    int n1 = n / 10000 % 10;
+    int n2 = n / 1000 % 10;
+    int n4 = n / 10 % 10;
+    int n5 = n % 10;
+    if (n > 99999 || n < 10000) Console.WriteLine("Неверное число");
+    else if (n1 == n5 && n2 == n4) Console.WriteLine("Введенное число - Паллиндром");
+    else Console.WriteLine("Введенное число не является паллиндром");
+}
+
+Console.WriteLine("Введите пятизначное число");
 int n = Convert.ToInt32(Console.ReadLine());
-int n1 = n / 10000 % 10;
-int n2 = n / 1000 % 10;
-int n4 = n / 10 % 10;
-int n5 = n % 10;
-if (n > 99999 || n < 10000) Console.WriteLine("Неверное число");
-else if (n1 == n5 && n2 == n4) Console.WriteLine("Введенное число - Паллиндром");
-else Console.WriteLine("Введенное число не является паллиндром");
+CheckPal(n);
---
+++ Seminar3/HW.Example23/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+void Cubes(int num)
+{
+    for (int i = 1; i <= num; i++)
+    {
+        Console.Write($"{i * i * i} ");
+    }
+}
+
+Console.WriteLine("введите целое число");
+int num = Convert.ToInt32(Console.ReadLine());
+Cubes(num);
