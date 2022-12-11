efab6804f49c13c419b5afa4968cecd10dbc0f25
artur <sro.artur@yandex.ru>
2022-12-11 22:57:14 +0300

    S5.HW3

--- Seminar5/HW.Example36/Program.cs
+++ Seminar5/HW.Example36/Program.cs
@@ -8,7 +8,7 @@ int[] CreateArray(int size)
 {
     int[] array = new int[size];
     for (int i = 0; i < size; i++)
-        array[i] = new Random().Next(-100, 101);
+        array[i] = new Random().Next(-100,101);
     return array;
 }
 
